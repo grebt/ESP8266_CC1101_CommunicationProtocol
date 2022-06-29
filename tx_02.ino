e582b84554d243df8d8a8217f5cf7e315abaf862
grebt <thomasgreb@gmx.net>
2022-06-29 21:28:00 +0200

    removed unused comments and information from templatefile

--- tx_02.ino
+++ tx_02.ino
@@ -1,7 +1,3 @@
-//New transmission method.
-//In addition, the gdo0 and gdo2 pin are not required.
-//https://github.com/LSatan/SmartRC-CC1101-Driver-Lib
-//by Little_S@tan
 #include <ELECHOUSE_CC1101_SRC_DRV.h>
 #include <SPI.h>
 #include <LED_Signal.hpp>
@@ -17,26 +13,4 @@ void loop() {
   while(loop){
     cc1101.rxTxDemo();
   }
-  //Serial.println("stopped");
-  //startCommunication();
-//3 different methods to send data without gdo
-//When sending, we give a little time to completely transmit the message (time in millis).
-//You can shorten the time. It depends on the data rate and the packet length. Just try it out for fine tuning.
-
-//Transmitt "Hello World" from byte format.
-//ELECHOUSE_cc1101.SendData(transmitt_byte, 11, 100);
-//sendSignal();
-//delay(2000);
-  
-//
-////Transmitt "Hello World" from char format.
-//ELECHOUSE_cc1101.SendData(transmitt_char, 100);
-//sendSignal();
-//
-////Transmitt "Hello World" from char format directly.
-//ELECHOUSE_cc1101.SendData("Hello World", 100);
-//sendSignal();
-//  Serial.println("RXBYTES:");
-//  Serial.println(ELECHOUSE_cc1101.SpiReadStatus(0x3B));
-//delay(2000);
 }
